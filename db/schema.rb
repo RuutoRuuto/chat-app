501d85293b9f4a94ccf0ad7fb89de07cc7902404
RuutoRuuto <n56g034y@gmail.com>
2020-11-21 18:21:01 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_125758) do
+ActiveRecord::Schema.define(version: 2020_11_21_060320) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "neame", null: false
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
